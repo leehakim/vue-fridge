5fe07df565f3b3df5ad973952aa52b6bc708735d
hsyoon <hsyoon@bstones.co.kr>
2023-04-14 16:43:15 +0900

    환경 셋팅 수정

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,39 +1,14 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import { NuxtConfig } from '@nuxt/types';
 
-// export default defineNuxtConfig({
-//   buildModules: ['@nuxtjs/style-resources'],
-//   styleResources: {
-//     scss: [
-//       'assets/style/_functions.scss',
-//       'assets/style/_fonts.scss',
-//       'assets/style/common.scss',
-//     ],
-//   },
+const defineNuxtConfig: NuxtConfig = {
+    buildModules: ['@nuxtjs/style-resources'],
+    styleResources: {
+        scss: ['~/assets/style/_functions.scss','~/assets/style/common.scss',],
+    },
+    // Nuxt.js 2.9 이전 버전에서 사용하던 옵션들
+    css:  ['~/assets/style/_functions.scss','~/assets/style/common.scss',],
+    // plugins: ['~/plugins/axios'],
+    // …
+};
 
-//   vite: {
-//     css: {
-//       preprocessorOptions: {
-//         scss: {
-//           additionalData: '@use "@/assets/_colors.scss" as *;',
-//         },
-//       },
-//     },
-//   },
-  
-// });
-import { defineNuxtConfig, NuxtOptionsBuildModules } from '@nuxt/types';
-
-// import { defineNuxtConfig, NuxtOptionsBuildModules } from 'nuxt3';
-
-const buildModules: NuxtOptionsBuildModules[] = ['@nuxtjs/style-resources'];
-
-export default defineNuxtConfig({
-  buildModules,
-  styleResources: {
-    scss: ['~/assets/scss/main.scss'],
-  },
-  // Nuxt.js 2.9 이전 버전에서 사용하던 옵션들
-  css: ['~/assets/css/main.css'],
-  plugins: ['~/plugins/axios'],
-  // …
-});
+export default defineNuxtConfig;
