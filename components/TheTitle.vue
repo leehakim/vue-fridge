0a11cc8f9071340ac5856f6551ae89135f17d5c4
leehakim <leehakim0520@gmail.com>
2023-04-14 15:52:53 +0900

    [wip] setting

--- components/TheTitle.vue
+++ components/TheTitle.vue
@@ -1,6 +1,20 @@
 <template>
   <section class="title">
-    <p>Title Area</p>
+    <picture>
+      <source
+        srcset="/title_bg_pc.png"
+        media="all and (min-width: 769px)"
+      />
+      <!-- <img src="assets/images/gnb_mo.png" alt="" /> -->
+    </picture>
+    <div class="text-box">
+      <p class="tit"><span class="en-b">2022</span> 삼성전자 신상 컬렉션</p>
+      <p class="desc">
+        올해 새롭게 선보이는 삼성 TV부터 BESPOKE 주방가전, 생활가전까지
+        <br />
+        모든 신제품을 삼성닷컴 특별 혜택으로 가장 먼저 만나보세요
+      </p>
+    </div>
   </section>
 </template>
 
@@ -15,5 +29,17 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+  .text-box {
+    text-align: center;
+    .tit {
+      font-size: 48px;
+      letter-spacing: -0.05em;
+    }
+    .desc {
+      color: #666;
+      font-size: 22px;
+      line-height: 38px;
+      letter-spacing: -0.025em;
+    }
+  }
 </style>
