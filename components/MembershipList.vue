c0d5f065c02020e288ac2217206dc499bf083653
dongseung <ttjh92@hanmail.net>
2023-04-18 17:24:32 +0900

    정렬확인

--- components/MembershipList.vue
+++ components/MembershipList.vue
@@ -9,25 +9,27 @@
 			<ul>
 				<li>
 					<MembershipListItem idx="1">
-						<template #tit> 삼성전자 멤버십<br />최대 26만 포인트 적립</template>
+						<template #tit>삼성전자 멤버십<br />최대 26만 포인트 적립</template>
 					</MembershipListItem>
 				</li>
 				<li>
 					<MembershipListItem idx="2">
-						<template #tit> 삼성닷컴 회원 제휴카드<br />결제 시 최대 3% 할인 </template>
+						<template #tit
+							>삼성닷컴 회원 제휴카드<br />결제 시 최대 3% 할인</template
+						>
 					</MembershipListItem>
 				</li>
 				<li>
 					<MembershipListItem idx="3">
 						<template #tit> 고객이 원하는 배송일<br />지정 서비스</template>
-						<template #disclaimer>
-							일부 제품 제외
-						</template>
+						<template #disclaimer> 일부 제품 제외 </template>
 					</MembershipListItem>
 				</li>
 				<li>
 					<MembershipListItem idx="4">
-						<template #tit> 삼성닷컴 제품 구매 시<br />전제품 배송비 무료</template>
+						<template #tit>
+							삼성닷컴 제품 구매 시<br />전제품 배송비 무료</template
+						>
 					</MembershipListItem>
 				</li>
 			</ul>
@@ -63,24 +65,24 @@ export default {
 		letter-spacing: -0.025em;
 		text-align: center;
 	}
-	.btn-wrap{
+	.btn-wrap {
 		display: flex;
 		align-items: center;
 		justify-content: center;
-		.sign-up{
+		.sign-up {
 			display: flex;
 			align-items: center;
 			justify-content: center;
-			width:vw-pc(138);
+			width: vw-pc(138);
 			height: vw-pc(38);
 			margin-right: vw-pc(32);
-			border-radius:vw-pc(19);
+			border-radius: vw-pc(19);
 			background: #000000;
 			font-size: vw-pc(16);
 			font-weight: 700;
 			color: #ffffff;
 		}
-		.more{
+		.more {
 			position: relative;
 			padding-bottom: vw-pc(5);
 			padding-right: vw-pc(17);
@@ -88,12 +90,12 @@ export default {
 			font-size: vw-pc(18);
 			font-weight: 700;
 			letter-spacing: -0.025em;
-			&::after{
-				content:'';
+			&::after {
+				content: '';
 				position: absolute;
 				right: 0;
 				bottom: vw-pc(9);
-				width:6px;
+				width: 6px;
 				height: 10px;
 				background: url('/public/arrow_right_pc.png') no-repeat center;
 			}
@@ -103,18 +105,18 @@ export default {
 		display: flex;
 		justify-content: space-between;
 		margin-top: vw-pc(40);
-		li{
+		li {
 			border-radius: vw-pc(10);
-			&:nth-child(1){
+			&:nth-child(1) {
 				background: #efdfbb;
 			}
-			&:nth-child(2){
+			&:nth-child(2) {
 				background: #c6e3f1;
 			}
-			&:nth-child(3){
+			&:nth-child(3) {
 				background: #bfede2;
 			}
-			&:nth-child(4){
+			&:nth-child(4) {
 				background: #c0d6f4;
 			}
 		}
