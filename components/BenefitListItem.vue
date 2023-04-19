625aeaf6551604d8799f59afc2fa6a1b71d1daa6
leehakim <leehakim0520@gmail.com>
2023-04-19 16:04:56 +0900

    style: [구매혜택] 클래스 네이밍

--- components/BenefitListItem.vue
+++ components/BenefitListItem.vue
@@ -1,6 +1,6 @@
 <template>
-	<div class="benefit-list-item">
-		<picture>
+	<div class="benefit_list_item">
+		<picture class="img_box">
 			<source
 				:srcset="'/benefit_bg_0' + benefit.idx + '_pc.png'"
 				media="all and (min-width: 769px)"
@@ -10,7 +10,7 @@
 		<div class="tag">
 			<p>{{ benefit.tag }}</p>
 		</div>
-		<div class="text-box">
+		<div class="text_box">
 			<p class="tit">{{ benefit.title }}</p>
 			<p class="copy">{{ benefit.copy }}</p>
 			<a v-if="benefit.cta" href="/" class="cta">{{ benefit.cta }}</a>
