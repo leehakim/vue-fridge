b614a8ce5cfd8a59e70cec8cbd31f4cc55ce3886
leehakim <leehakim0520@gmail.com>
2023-04-19 18:25:11 +0900

    design: [구매혜택] mo 스타일 & swiper

--- components/BenefitList.vue
+++ components/BenefitList.vue
@@ -1,42 +1,45 @@
 <template>
-	<section class="section benefit">
-		<div class="section_container">
-			<p class="title">런칭 구매 혜택</p>
-			<swiper :slides-per-view="3">
-				<SwiperSlide v-for="benefit in benefits" :key="benefit.idx">
-					<BenefitListItem :benefit="benefit"></BenefitListItem>
-				</SwiperSlide>
-			</swiper>
-			<div class="center_box">
-				<button class="toggle_btn" @click="show = !show">
-					혜택 유의사항을 꼭 확인하세요
-				</button>
-				<div v-if="show" class="toggle_contents">
-					<p>혜택 유의 사항</p>
-				</div>
-			</div>
-		</div>
-		{{ benefitData }}
-	</section>
+  <section class="section benefit">
+    <div class="section_container">
+      <p class="title">런칭 구매 혜택</p>
+      <swiper
+        :slides-per-view="1.5"
+        :breakpoints="{ 769: { slidesPerView: 3 } }"
+      >
+        <SwiperSlide v-for="benefit in benefits" :key="benefit.idx">
+          <BenefitListItem :benefit="benefit"></BenefitListItem>
+        </SwiperSlide>
+      </swiper>
+      <div class="center_box">
+        <button class="toggle_btn" @click="show = !show">
+          혜택 유의사항을 꼭 확인하세요
+        </button>
+        <div v-if="show" class="toggle_contents">
+          <p>혜택 유의 사항</p>
+        </div>
+      </div>
+    </div>
+    {{ benefitData }}
+  </section>
 </template>
 
 <script>
-import BenefitListItem from './BenefitListItem.vue';
-import benefitData from '~/json-data/BenefitList.json';
-import { Swiper, SwiperSlide } from 'swiper/vue';
-import 'swiper/css';
+import BenefitListItem from "./BenefitListItem.vue";
+import BenefitData from "~/json-data/BenefitList.json";
+import { Swiper, SwiperSlide } from "swiper/vue";
+import "swiper/css";
 
 export default {
-	name: 'BenefitList',
-	components: { BenefitListItem, Swiper, SwiperSlide },
-	setup() {
-		const show = ref(false);
-		const benefits = benefitData.benefit_list;
-		return { show, benefits };
-	},
+  name: "BenefitList",
+  components: { BenefitListItem, Swiper, SwiperSlide },
+  setup() {
+    const show = ref(false);
+    const benefits = BenefitData.benefit_list;
+    return { show, benefits };
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-@import '@/assets/style/benefit.scss';
+@import "@/assets/style/benefit.scss";
 </style>
--- components/BenefitListItem.vue
+++ components/BenefitListItem.vue
@@ -1,49 +1,49 @@
 <template>
-	<div class="benefit_list_item">
-		<picture class="img_box">
-			<source
-				:srcset="'/benefit_bg_0' + benefit.idx + '_pc.png'"
-				media="all and (min-width: 769px)"
-			/>
-			<img :src="'/benefit_bg_0' + benefit.idx + '_mo.png'" alt="" />
-		</picture>
-		<div class="tag">
-			<p>{{ benefit.tag }}</p>
-		</div>
-		<div class="text_box">
-			<p class="tit">{{ benefit.title }}</p>
-			<p class="copy">{{ benefit.copy }}</p>
-			<a v-if="benefit.cta" href="/" class="cta">{{ benefit.cta }}</a>
-			<p v-if="benefit.disclaimer" class="disclaimer">
-				{{ benefit.disclaimer }}
-			</p>
-		</div>
-	</div>
+  <div class="benefit_list_item">
+    <picture class="img_box">
+      <source
+        :srcset="'/benefit_bg_0' + benefit.idx + '_pc.png'"
+        media="all and (min-width: 769px)"
+      />
+      <img :src="'/benefit_bg_0' + benefit.idx + '_mo.png'" alt="" />
+    </picture>
+    <div class="tag">
+      <p>{{ benefit.tag }}</p>
+    </div>
+    <div class="text_box">
+      <p class="tit">{{ benefit.title }}</p>
+      <p class="copy">{{ benefit.copy }}</p>
+      <a v-if="benefit.cta" href="/" class="cta">{{ benefit.cta }}</a>
+      <p v-if="benefit.disclaimer" class="disclaimer">
+        {{ benefit.disclaimer }}
+      </p>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-	props: {
-		benefit: {
-			type: Object,
-			default() {
-				return {
-					idx: 1,
-					tag: 'tag',
-					title: 'title',
-					copy: 'copy',
-					cta: 'cta',
-					disclaimer: 'disclaimer',
-				};
-			},
-		},
-	},
-	setup(props) {
-		return {};
-	},
+  props: {
+    benefit: {
+      type: Object,
+      default() {
+        return {
+          idx: 1,
+          tag: "tag",
+          title: "title",
+          copy: "copy",
+          cta: "cta",
+          disclaimer: "disclaimer",
+        };
+      },
+    },
+  },
+  setup(props) {
+    return {};
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-@import '@/assets/style/benefit.scss';
+@import "@/assets/style/benefit.scss";
 </style>
