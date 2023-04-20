f4bf50f5525ca17527510de389810752fb767bc5
leehakim <leehakim0520@gmail.com>
2023-04-20 10:20:11 +0900

    style: [kv, title] scss 별도 생성

--- components/KeyVisual.vue
+++ components/KeyVisual.vue
@@ -1,65 +1,24 @@
 <template>
-	<section class="section kv">
-		<picture>
-			<source srcset="/kv_bg_pc.png" media="all and (min-width: 769px)" />
-			<img src="/kv_bg_mo.png" alt="" />
-		</picture>
-		<div class="text-box">
-			<p class="tit">BESPOKE 와인냉장고<br />런칭</p>
-			<p class="desc">와인 보관부터 관리, 푸드 페어링까지 전문적이고 쉽게</p>
-		</div>
-	</section>
+  <section class="section kv">
+    <picture>
+      <source srcset="/kv_bg_pc.png" media="all and (min-width: 769px)" />
+      <img src="/kv_bg_mo.png" alt="kv bg" />
+    </picture>
+    <div class="text-box">
+      <p class="tit">BESPOKE 와인냉장고<br />런칭</p>
+      <p class="desc">와인 보관부터 관리, 푸드 페어링까지 전문적이고 쉽게</p>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
-	setup() {
-		return {};
-	},
+  setup() {
+    return {};
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-.kv {
-	.text-box {
-		position: absolute;
-		top: 50%;
-		left: vw-pc(296);
-		transform: translateY(-50%);
-		color: #fff;
-	}
-	.tit {
-		font-size: vw-pc(54);
-		line-height: vw-pc(55);
-		font-weight: 700;
-	}
-	.desc {
-		margin-top: vw-pc(35);
-		font-size: vw-pc(24);
-		letter-spacing: -0.025em;
-		line-height: vw-pc(35);
-	}
-}
-
-@media (max-width: 768px) {
-	.kv {
-		.text-box {
-			top: vw-mo(133);
-			left: 0;
-			transform: none;
-			width: 100%;
-			text-align: center;
-		}
-		.tit {
-			font-size: vw-mo(64);
-			line-height: vw-mo(70);
-		}
-		.desc {
-			margin-top: vw-mo(38);
-			font-size: vw-mo(26);
-			letter-spacing: -0.02em;
-			line-height: vw-mo(34);
-		}
-	}
-}
+@import "@/assets/style/kv.scss";
 </style>
--- components/TheTitle.vue
+++ components/TheTitle.vue
@@ -1,65 +1,30 @@
 <template>
-	<section class="section title">
-		<picture>
-			<source srcset="/title_bg_pc.png" media="all and (min-width: 769px)" />
-			<img src="/title_bg_mo.png" alt="" />
-		</picture>
-		<div class="text-box">
-			<p class="tit">
-				<span class="en-b">2022</span> 삼성전자 <br class="m-show" />신상 컬렉션
-			</p>
-			<p class="desc">
-				올해 새롭게 선보이는 삼성 TV부터 BESPOKE 주방가전, 생활가전까지
-				<br />
-				모든 신제품을 삼성닷컴 특별 혜택으로 가장 먼저 만나보세요
-			</p>
-		</div>
-	</section>
+  <section class="section title">
+    <picture>
+      <source srcset="/title_bg_pc.png" media="all and (min-width: 769px)" />
+      <img src="/title_bg_mo.png" alt="title bg" />
+    </picture>
+    <div class="text-box">
+      <p class="tit">
+        <span class="en-b">2022</span> 삼성전자 <br class="m-show" />신상 컬렉션
+      </p>
+      <p class="desc">
+        올해 새롭게 선보이는 삼성 TV부터 BESPOKE 주방가전, 생활가전까지
+        <br />
+        모든 신제품을 삼성닷컴 특별 혜택으로 가장 먼저 만나보세요
+      </p>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
-	setup() {
-		return {};
-	},
+  setup() {
+    return {};
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-.title {
-	.text-box {
-		position: absolute;
-		top: 50%;
-		left: 0;
-		transform: translateY(-50%);
-		width: 100%;
-		text-align: center;
-	}
-	.tit {
-		font-size: vw-pc(48);
-		letter-spacing: -0.05em;
-	}
-	.desc {
-		margin-top: vw-pc(39);
-		color: #666;
-		font-size: vw-pc(22);
-		line-height: vw-pc(38);
-		letter-spacing: -0.025em;
-	}
-}
-
-@media (max-width: 768px) {
-	.title {
-		.tit {
-			font-size: vw-mo(72);
-			line-height: vw-mo(86);
-		}
-		.desc {
-			margin-top: vw-mo(68);
-			font-size: vw-mo(24);
-			line-height: vw-mo(38);
-			letter-spacing: -0.05em;
-		}
-	}
-}
+@import "@/assets/style/title.scss";
 </style>
