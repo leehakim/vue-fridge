48ef741403391233c3f0983f79b11ba45b157806
leehakim <leehakim0520@gmail.com>
2023-04-20 12:12:22 +0900

    style: [benefit] wip

--- components/BenefitList.vue
+++ components/BenefitList.vue
@@ -11,15 +11,20 @@
         </SwiperSlide>
       </swiper>
       <div class="center_box">
-        <button class="toggle_btn" @click="show = !show">
+        <button
+          class="toggle_arrow toggle_btn"
+          :class="{ open: show }"
+          @click="show = !show"
+        >
           혜택 유의사항을 꼭 확인하세요
         </button>
-        <div v-if="show" class="toggle_contents">
-          <p>혜택 유의 사항</p>
+        <div v-if="show">
+          <div class="toggle_contents">
+            <p>혜택 유의 사항</p>
+          </div>
         </div>
       </div>
     </div>
-    {{ benefitData }}
   </section>
 </template>
 
