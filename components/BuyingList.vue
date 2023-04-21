e130559103158016f64175f9ebff0533f8c71e1d
leehakim <leehakim0520@gmail.com>
2023-04-21 17:23:45 +0900

    feat: [buying] wip

--- components/BuyingList.vue
+++ components/BuyingList.vue
@@ -21,22 +21,30 @@
         >
           <SwiperSlide v-for="(data, idx) in BuyingData" :key="idx">
             <div class="buying_item">
-              <div class="text_box">
-                <p class="color">{{ data.color }}</p>
-                <p class="prd_num">{{ data.prd_num }}</p>
-                <hr />
-                <dl class="price">
-                  <dt>판매가</dt>
-                  <dd>
-                    <span>{{ data.price }}</span> 원
-                  </dd>
-                </dl>
-                <dl class="benefit">
-                  <dt>혜택가</dt>
-                  <dd>
-                    <span>{{ data.benefit }}</span> 원
-                  </dd>
-                </dl>
+              <div class="buying_item_container">
+                <div class="text_box">
+                  <p class="color">{{ data.color }}</p>
+                  <p class="prd_num">{{ data.prd_num }}</p>
+                  <hr class="divider" />
+                  <dl class="price">
+                    <dt>판매가</dt>
+                    <dd>
+                      <span>{{ data.price }}</span> 원
+                    </dd>
+                  </dl>
+                  <dl class="benefit">
+                    <dt>혜택가</dt>
+                    <dd>
+                      <span>{{ data.benefit }}</span> 원
+                    </dd>
+                  </dl>
+                </div>
+              </div>
+              <div v-if="data.new" class="tag new">
+                <p><span class="en_b">NEW</span>컬러</p>
+              </div>
+              <div v-if="data.best" class="tag best">
+                <p><span class="en_b">BEST</span></p>
               </div>
             </div>
           </SwiperSlide>
