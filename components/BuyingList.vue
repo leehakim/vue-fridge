51329dd355ac38d3895344bff714b188ac0dcf80
Byeon Unseop <ghlejr316@naver.com>
2023-04-24 16:15:57 +0900

    style: [바잉툴영역] mo 중간커밋

--- components/BuyingList.vue
+++ components/BuyingList.vue
@@ -2,13 +2,13 @@
   <section class="section buying">
     <div class="section_container">
       <div class="section_top">
-        <p class="title">나에게 딱 맞는 컬러와 소재를 골라 조합해보세요</p>
+        <p class="title">나에게 딱 맞는 컬러와 소재를 <br class="m_show">골라 조합해보세요</p>
         <button type="button" class="common_button black_bg btn_custom">
           나만의 와인냉장고 조합하기
         </button>
       </div>
       <div class="buying_swiper">
-        <swiper v-bind="swiperOptions">
+        <swiper v-bind="swiperOptions" @slideChange="onSlideChange" @buyingSwiper="buyingSwiper">
           <SwiperSlide v-for="(data, idx) in BuyingData" :key="idx">
             <BuyingListItem :item="data" />
           </SwiperSlide>
@@ -26,13 +26,13 @@
 
 <script>
 import BuyingData from "~/json-data/BuyingList.json";
-
 import { Swiper, SwiperSlide } from "swiper/vue";
 import "swiper/scss";
-import SwiperCore, { Pagination, Navigation } from "swiper";
+import SwiperCore, { Pagination, Navigation, Autoplay } from "swiper";
 import "swiper/scss/navigation";
 import "swiper/scss/pagination";
-SwiperCore.use([Pagination, Navigation]);
+import $ from 'jquery'
+SwiperCore.use([Pagination, Navigation, Autoplay]);
 
 import BuyingListItem from "./BuyingListitem.vue"
 export default {
@@ -46,12 +46,52 @@ export default {
           prevEl: ".buying .swiper-button-prev",
           nextEl: ".buying .swiper-button-next",
         },
+        autoplay: {
+          delay:2500,
+          disableOnInteraction: false
+        },
         pagination: {
           el: ".buying .swiper-pagination",
-          clickable: true
+          clickable: true,
         },
+        breakpoints: {
+          769: {
+            autoplay: false,
+          }
+        }
       },
     };
   },
-};
+  methods: {
+    onSlideChange: () => {
+      var $bulletActive = $('.buying .swiper-pagination-bullet-active');
+      $bulletActive.append('<span></span>').siblings().children().remove()
+      console.log('시작');
+    },
+    slidePlayButton: () => {
+      $('.play_btn').on('click', function() {
+        var isSwiperPlay = 0;
+       
+        if(isSwiperPlay==0){
+            $('.play_btn').addClass('stop').removeClass('play');
+            Swiper.autoplay.stop();
+            $('.swiper-pagination-bullet-active span').css('animation-play-state', 'paused')
+            isSwiperPlay = 1;
+            console.log(isSwiperPlay);
+        }else {
+            $('.play_btn').addClass('play').removeClass('stop');
+            Swiper.autoplay.start();
+            $('.swiper-pagination-bullet-active span').css('animation-play-state', 'running')
+            isSwiperPlay = 0;
+            console.log(isSwiperPlay);
+        }
+      })
+    },
+  },
+  mounted() {
+    var swiperPaination = $('.swiper-pagination')
+    swiperPaination.append('<span class="play_btn play"></span>');
+    this.slidePlayButton()
+  }
+}
 </script>
